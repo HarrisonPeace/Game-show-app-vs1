13cd45e2c9fb85a1015e83a42d2a94b85c0ed3bd
Harrison Peace <harrison@apwines.com>
2020-10-16 14:35:54 +1100

    Application working - need to update css and re-work Javascript code

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 // JavaScript Document
 
 const startGameBtn = document.querySelector('.btn-start');
-const gameOverlay = document.querySelector('#overlay');
+const restartGameBtn = document.querySelectorAll('.btn-restart');
+const gameOverlay = document.querySelectorAll('.overlay');
 const phraseUl = document.querySelector('#phrase');
 const qwerty = document.querySelector('#qwerty');
+let letterFound = 1;
+let wrongGuess = 0;
+const lives = document.querySelectorAll('.tries img');
 const phrases = [
-	'you will never touch this',
-	'its just a flesh wound',
-	'im in a glass case of emotion',
-	'excuse me i belive you have my stapler',
-	'show me the money',
-	'to infinity and beyond',
-	'shaken, not stirred',
-	'houston we have a problem'
+	'You will never touch this',
+	'Its just a flesh wound',
+	'Im in a glass case of emotion',
+	'Excuse me i belive you have my stapler',
+	'Show me the money',
+	'To infinity and beyond',
+	'Shaken not stirred',
+	'Houston we have a problem'
 ]
 
 function getRandomArray (array) {
@@ -40,17 +44,31 @@ function addStringToDisplay (string) {
 
 function checkLetter (letter) {
 	const letterArray = document.querySelectorAll('.letter')
+	let letterTracker = 0;
 	for (let i = 0; i < letterArray.length; i++) {
-		if (letterArray[i].innerHTML === letter) {
-			letterArray[i].style.color = 'black';
+		if (letterArray[i].innerHTML.toLowerCase() === letter) {
+			letterArray[i].classList.add('show');
+			letterTracker++;
 		}
+	}
+    if (letterTracker <= 0) {
+		letterFound = 0;
+    } else {
+		letterFound = 1;
+	}
+
+}
+
+function removeLives (guess) {
+	if (guess >= 0) {
+		lives[(guess - 1)].src = "images/lostHeart.png"
 	}
 }
 
 addStringToDisplay(getRandomArray(phrases));
 
 startGameBtn.addEventListener('click', () => {
-	gameOverlay.style.display = 'none';
+	gameOverlay[0].style.display = 'none';
 });
 
 qwerty.addEventListener('click', (event) => {
@@ -60,5 +78,58 @@ qwerty.addEventListener('click', (event) => {
 	if (isLetter(letterClicked)) {
 	buttonClicked.setAttribute('disabled', '');
 	buttonClicked.style.opacity = '0.5';
+	if (letterFound === 0) {
+		wrongGuess++;
+		removeLives(wrongGuess);
+    }
+	letterFound = 1;
+	checkWinLose();
 	}
-}) 
+    
+})
+
+function checkWinLose () {
+	const letters = document.querySelectorAll('.letter');
+	const lettersSelected = document.querySelectorAll('.show')
+	if (lettersSelected.length === letters.length) {
+		document.querySelector('#win').style.display = 'flex'
+	} else if (wrongGuess === 5) {
+		document.querySelector('#lose').style.display = 'flex'
+	}
+}
+
+restartGameBtn[0].addEventListener('click', () => {
+	gameOverlay[1].style.display = 'none';
+	wrongGuess = 0;
+	const disabledButtons = document.querySelectorAll('[disabled]')
+	const phraseletters = document.querySelectorAll('.letter, .space')
+	for (let i = 0; i < disabledButtons.length; i++) {
+		disabledButtons[i].removeAttribute('disabled', '');
+		disabledButtons[i].style.opacity = '1';
+	}
+	for (let i = 0; i < lives.length; i++) {
+		lives[i].src = "images/liveHeart.png"
+	}
+    for (let i = 0; i < phraseletters.length; i++) {
+    phraseletters[i].remove();
+	} 
+	addStringToDisplay(getRandomArray(phrases));
+})
+
+restartGameBtn[1].addEventListener('click', () => {
+	gameOverlay[2].style.display = 'none';
+	wrongGuess = 0;
+	const disabledButtons = document.querySelectorAll('[disabled]')
+	const phraseletters = document.querySelectorAll('.letter, .space')
+	for (let i = 0; i < disabledButtons.length; i++) {
+		disabledButtons[i].removeAttribute('disabled', '');
+		disabledButtons[i].style.opacity = '1';
+	}
+	for (let i = 0; i < lives.length; i++) {
+		lives[i].src = "images/liveHeart.png"
+	}
+    for (let i = 0; i < phraseletters.length; i++) {
+    phraseletters[i].remove();
+	} 
+	addStringToDisplay(getRandomArray(phrases));
+})
